7a2a25fe4c86bd6f6bd9567bac6ed312bbf2e80d
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-06-12 12:47:26 +0100

    added remove edges script

--- remove_edges_script.sh
+++ remove_edges_script.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-for dataset in cora_ml citeseer ppi pubmed mit
-do
-	for seed in {0..29} 
-	do
-		edgelist=datasets/${dataset}/edgelist.tsv
-		features=datasets/${dataset}/feats.csv
-		labels=datasets/${dataset}/labels.csv
-		output=edgelists/${dataset}/
-		python remove_edges.py --edgelist=$edgelist --features=$features --labels=$labels --output=$output --seed $seed
-	done
-done
+#SBATCH --job-name=removeEdges
+#SBATCH --output=removeEdges%A_%a.out
+#SBATCH --error=removeEdges%A_%a.err
+#SBATCH --array=0-149
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --mail-type ALL
+
+datasets=({cora_ml,citeseer,ppi,pubmed,mit})
+seeds=({0..29})
+
+num_datasets=${#datasets[@]}
+num_seeds=${#seeds[@]}
+
+dataset_id=$((SLURM_ARRAY_TASK_ID / num_seeds % num_datasets ))
+seed_id=$((SLURM_ARRAY_TASK_ID % (num_seed) ))
+
+dataset=${datasets[$dataset_id]}
+seed=${seeds[$seed_id]}
+
+edgelist=datasets/${dataset}/edgelist.tsv
+features=datasets/${dataset}/feats.csv
+labels=datasets/${dataset}/labels.csv
+output=edgelists/${dataset}/
+python remove_edges.py --edgelist=$edgelist --features=$features --labels=$labels --output=$output --seed $seed
