0a530534d5cb18382fd08cacc253167a8a01549e
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-06-24 10:58:32 +0100

    slurm array fix

--- remove_edges_script.sh
+++ remove_edges_script.sh
@@ -4,10 +4,10 @@
 #SBATCH --output=removeEdges_%A_%a.out
 #SBATCH --error=removeEdges_%A_%a.err
 #SBATCH --array=0-149
-#SBATCH --time=01:00:00
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=16G
-#SBATCH --mail-type ALL
+#SBATCH --mem=20G
+# SBATCH --mail-type ALL
 
 datasets=({cora_ml,citeseer,ppi,pubmed,mit})
 seeds=({0..29})
@@ -16,7 +16,7 @@ num_datasets=${#datasets[@]}
 num_seeds=${#seeds[@]}
 
 dataset_id=$((SLURM_ARRAY_TASK_ID / num_seeds % num_datasets ))
-seed_id=$((SLURM_ARRAY_TASK_ID % (num_seed) ))
+seed_id=$((SLURM_ARRAY_TASK_ID % (num_seeds) ))
 
 dataset=${datasets[$dataset_id]}
 seed=${seeds[$seed_id]}
@@ -24,10 +24,15 @@ seed=${seeds[$seed_id]}
 edgelist=datasets/${dataset}/edgelist.tsv
 features=datasets/${dataset}/feats.csv
 labels=datasets/${dataset}/labels.csv
-output=edgelists/${dataset}/
+output=edgelists/${dataset}
 
-module purge
-module load bluebear
-module load apps/python3/3.5.2
+edgelist_f=$(printf "${output}/seed=%03d/training_edges/edgelist.tsv" ${seed} )
+# echo $edgelist_f
+if [ ! -f $edgelist_f  ]
+then
+	module purge
+	module load bluebear
+	module load apps/python3/3.5.2
 
-python remove_edges.py --edgelist=$edgelist --features=$features --labels=$labels --output=$output --seed $seed
+	python remove_edges.py --edgelist=$edgelist --features=$features --labels=$labels --output=$output --seed $seed
+fi
