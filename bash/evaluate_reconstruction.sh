909d7a3bbb970696e1329e3d33c0264846eeac7b
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-11-15 11:29:25 +0000

    added evaluation scripts for lp and recon

--- bash/evaluate_reconstruction.sh
+++ bash/evaluate_reconstruction.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=evaluateRECON_%A_%a.out
 #SBATCH --error=evaluateRECON_%A_%a.err
 #SBATCH --array=0-3599
-#SBATCH --time=3-00:00:00
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=32G
+#SBATCH --mem=20G
 
 # heat=/rds/projects/2018/hesz01/heat/main.py
 e=5
