adbb68031e36fb532ce8b63cc2064072540ae23b
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2020-02-06 10:00:19 +0000

    updated remove script

--- remove_edges.py
+++ remove_edges.py
@@ -7,28 +7,79 @@ import networkx as nx
 
 import argparse
 
-from heat.utils import load_data
+from hednet.utils import load_data
+from remove_utils import sample_non_edges, write_edgelist_to_file
 
-def write_edgelist_to_file(edgelist, file):
-	with open(file, "w+") as f:
-		for u, v in edgelist:
-			f.write("{}\t{}\n".format(u, v))
-
-def split_edges(edges, non_edges, seed, val_split=0.00, test_split=0.15, neg_mul=1):
+def split_edges(graph, 
+	edges, 
+	seed,
+	val_split=0.05, 
+	test_split=0.10, 
+	neg_mul=1,
+	cover=False):
 	
+	assert isinstance(graph, nx.DiGraph)
+	assert isinstance(edges, list)
+
+	edge_set = set(edges)
+
 	num_val_edges = int(np.ceil(len(edges) * val_split))
 	num_test_edges = int(np.ceil(len(edges) * test_split))
 
 	random.seed(seed)
 	random.shuffle(edges)
-	random.shuffle(non_edges)
+	# random.shuffle(non_edges)
 
-	val_edges = edges[:num_val_edges]
-	test_edges = edges[num_val_edges:num_val_edges+num_test_edges]
-	train_edges = edges[num_val_edges+num_test_edges:]
+	# ensure every node appears in edgelist
+	nodes = set(graph)
+	# edges = set(edges)
+	cover = []
+	if cover:
+		for u, v in edges:
+			if u in nodes or v in nodes:
+				nodes -= {u, v}
+				cover.append((u, v))
+			if len(nodes) == 0:
+				break
+		
+		print ("determined cover")
 
-	val_non_edges = non_edges[:num_val_edges*neg_mul]
-	test_non_edges = non_edges[num_val_edges*neg_mul:num_val_edges*neg_mul+num_test_edges*neg_mul]
+	# edges = [edge for edge in edges
+	# 	if edge not in cover] + cover
+	edges = filter(lambda edge: edge not in cover, edges)
+	
+	print ("filtered cover out of edges")
+	# edges = list(edges - cover) + list(cover)
+
+	# val_edges = edges[:num_val_edges]
+	# test_edges = edges[num_val_edges:num_val_edges+num_test_edges]
+	# train_edges = edges[num_val_edges+num_test_edges:]
+	val_edges = []
+	test_edges = []
+	train_edges = []
+	for edge in edges:
+		if len(val_edges) < num_val_edges:
+			val_edges.append(edge)
+		elif len(test_edges) < num_test_edges:
+			test_edges.append(edge)
+		else:
+			train_edges.append(edge)
+
+	train_edges += cover
+
+	print ("determined edge split")
+
+	# val_non_edges = non_edges[:num_val_edges*neg_mul]
+	# test_non_edges = non_edges[num_val_edges*neg_mul:num_val_edges*neg_mul+num_test_edges*neg_mul]
+
+	val_non_edges = sample_non_edges(graph, 
+		edge_set, 
+		num_val_edges*neg_mul)
+	print ("determined val non edges")
+	test_non_edges = sample_non_edges(graph,
+		edge_set.union(val_non_edges),
+		num_test_edges*neg_mul)
+	print ("determined test non edges")
 
 	return train_edges, (val_edges, val_non_edges), (test_edges, test_non_edges)
 
@@ -38,16 +89,17 @@ def parse_args():
 	'''
 	parser = argparse.ArgumentParser(description="Script to remove edges for link prediction experiments")
 
-	parser.add_argument("--edgelist", dest="edgelist", type=str, default="datasets/cora_ml/edgelist.tsv",
+	parser.add_argument("--edgelist", dest="edgelist", type=str, 
 		help="edgelist to load.")
-	parser.add_argument("--features", dest="features", type=str, default="datasets/cora_ml/feats.csv",
+	parser.add_argument("--features", dest="features", type=str, 
 		help="features to load.")
-	parser.add_argument("--labels", dest="labels", type=str, default="datasets/cora_ml/labels.csv",
+	parser.add_argument("--labels", dest="labels", type=str, 
 		help="path to labels")
-	parser.add_argument("--output", dest="output", type=str, default="edgelists/cora_ml",
+	parser.add_argument("--output", dest="output", type=str, 
 		help="path to save training and removed edges")
 
-	parser.add_argument('--directed', action="store_true", help='flag to train on directed graph')
+	parser.add_argument('--directed', 
+		action="store_true", help='flag to train on directed graph')
 
 	parser.add_argument("--seed", type=int, default=0)
 
@@ -57,6 +109,7 @@ def parse_args():
 def main():
 
 	args = parse_args()
+	args.directed = True
 
 	seed= args.seed
 	training_edgelist_dir = os.path.join(args.output, "seed={:03d}".format(seed), "training_edges")
@@ -75,16 +128,23 @@ def main():
 	
 	graph, _, _ = load_data(args)
 	print("loaded dataset")
+	assert nx.is_directed(graph)
 
 	edges = list(graph.edges())
-	non_edges = list(nx.non_edges(graph))
+	print ("enumerated edges")
 
-	_, (val_edges, val_non_edges), (test_edges, test_non_edges) = split_edges(edges, non_edges, seed)
+	(_, (val_edges, val_non_edges), 
+	(test_edges, test_non_edges)) = split_edges(graph, 
+		edges, 
+		seed, 
+		val_split=0)
 
-	for edge in test_edges:
-		assert edge in graph.edges() or edge[::-1] in graph.edges()
+	print ("number of val edges", len(val_edges), 
+		"number of val non edges", len(val_edges))
+	print ("number of test edges", len(test_edges), 
+		"number of test non edges", len(test_edges))
 
-	graph.remove_edges_from(val_edges + test_edges)
+	graph.remove_edges_from(val_edges + test_edges) # remove val and test edges
 	graph.add_edges_from(((u, u, {"weight": 0}) for u in graph.nodes())) # ensure that every node appears at least once by adding self loops
 
 	print ("removed edges")
@@ -96,6 +156,6 @@ def main():
 	write_edgelist_to_file(test_non_edges, test_non_edgelist_fn)
 
 	print ("done")
-
+	
 if __name__ == "__main__":
 	main()
