b802c084d651e38c9e0c9e3be3d68de907e6b380
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2020-05-14 14:37:23 +0100

    update to collate results to compute sem

--- collate_results.py
+++ collate_results.py
@@ -2,6 +2,14 @@ import pandas as pd
 import argparse
 import os
 
+from scipy.stats import ttest_rel
+import itertools
+
+def make_dir(d):
+	if not os.path.exists(d):
+		print ("making directory", d)
+		os.makedirs(d, exist_ok=True)
+
 def parse_args():
 	'''
 	parse args from the command line
@@ -34,21 +42,28 @@ def main():
 
 	exp = "{}_experiment".format(args.exp)
 	dims = ["dim={:03}".format(dim) for dim in (5, 10, 25, 50)]
-	algorithms = ["nk"] + ["aane", "abrw", "tadw", 
-		"attrpure", "deepwalk", "sagegcn"] + \
-		["alpha={:.02f}".format(alpha) 
+	baseline_algs = ["nk"] + ["aane", "abrw", "tadw", 
+		"attrpure", "deepwalk", "sagegcn"] 
+	heat_algs = ["alpha={:.02f}".format(alpha) 
 			for alpha in (0, 0.05, 0.1, 0.2, 0.5, 1.0)]
 
+	algorithms = baseline_algs + heat_algs
+
 	output_dir = os.path.join(args.output, exp) 
-	if not os.path.exists(output_dir):
-		print ("making directory", output_dir)
-		os.makedirs(output_dir, exist_ok=True)
+	make_dir(output_dir)
 
-	for dataset in datasets:
 
-		for dim in dims:
-	
-			collated_df = pd.DataFrame()
+	for dim in dims:
+
+		output_dir_ = os.path.join(output_dir, 
+			dim)
+
+		for dataset in datasets:
+
+			mean_df = pd.DataFrame()
+			sem_df = pd.DataFrame()
+
+			dfs = dict()# store dfs for ttests
 
 			for algorithm in algorithms:
 
@@ -61,17 +76,44 @@ def main():
 				print ("reading", results_file)
 
 				results_df = pd.read_csv(results_file, index_col=0, sep=",")
-				assert results_df.shape[0] == num_seeds, (dataset, 
-					dim, algorithm)
+				assert results_df.shape[0] == num_seeds, \
+					(dataset, dim, algorithm)
 
-				collated_df = collated_df.append(pd.Series(
+				dfs[algorithm] = results_df
+
+				mean_df = collated_df.append(pd.Series(
 					results_df.mean(0), name=algorithm
 				))
 
-			output_filename = os.path.join(output_dir,
-				"{}_{}.csv".format(dataset, dim))
-			print ("writing to", output_filename)
-			collated_df.to_csv(output_filename)
+				sem_df = collated_df.append(pd.Series(
+					results_df.sem(0), name=algorithm
+				))
+
+			mean_filename = os.path.join(output_dir_,
+				"{}_{}_means.csv".format(dataset, dim))
+			print ("writing to", mean_filename)
+			mean_df.to_csv(mean_filename)
+
+			sem_filename = os.path.join(output_dir_,
+				"{}_{}_sems.csv".format(dataset, dim))
+			print ("writing to", sem_filename)
+			sem_df.to_csv(sem_filename)
+
+			# perform t tests
+			for a1, a2 in itertools.product(heat_algs, 
+				baseline_algs):
+
+				ttest = ttest_rel(dfs[a1], dfs[a2])
+				ttest_df = pd.DataFrame(ttest, 
+					columns=dfs[a1].columns,
+					index=["t-statistic", "p-value"])
+
+				ttest_df_filename = os.path.join(output_dir_,
+					"{}_{}_ttest-{}-{}.csv".format(dataset, dim,
+						a1, a2))
+				print ("writing ttests for", a1, "and", a2,
+					"to", ttest_df_filename)
+				ttest_df.to_csv(ttest_df_filename)
 
 
 if __name__ == "__main__":
